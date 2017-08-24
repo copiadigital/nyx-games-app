e76a0a00d1d23e1b096e3ffee40aa1e1970c34b0
Sam Bell <me@sam-bell.com>
2017-08-24 10:58:05 +0100

    HTML edits

--- src/App.js
+++ src/App.js
@@ -15,15 +15,25 @@ class App extends Component {
         <BrowserRouter>
           <div className="app">
             <div className="app-header">
-                <div>
+                <div className="wrapper wrapper--thin">
                     <img src={logo} className="app-logo" alt="NYX logo" />
-                    <h2>Game Portal</h2>
+                    <h2 className="app-title">Game Portal</h2>
+                    <ul className="nav">
+                        <li><a href="/news">News</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/service">Customer service</a></li>
+                    </ul>
                 </div>
             </div>
-            <div className="app-body">
-                <Route path="/" exact={true} render={() => <Redirect to="/games" />} />
-                <Route path="/games" component={Games} />
-                <Route path="/game/:gameId/:gameSlug" component={Game} />
+            <div className="banner">
+                <a className="banner-button">Find out now</a>
+            </div>
+            <div className="wrapper">
+                <div className="app-body">
+                    <Route path="/" exact={true} render={() => <Redirect to="/games" />} />
+                    <Route path="/games" component={Games} />
+                    <Route path="/game/:gameId/:gameSlug" component={Game} />
+                </div>
             </div>
             <div className="app-footer">
               <p>&copy; 2017</p>
--- src/components/games/FilterForm.js
+++ src/components/games/FilterForm.js
@@ -9,31 +9,33 @@ import ResetFilter from "./filters/ResetFilter";
 class FilterForm extends Component {
     render() {
         return (
-            <form className="game-filter">
-                <FeaturedFilter
-                    featured={ this.props.filter.featured }
-                    setFilter={ this.props.setFilter }
-                />
-                <JurisdictionFilter
-                    jurisdiction={ this.props.filter.jurisdiction }
-                    setFilter={ this.props.setFilter }
-                />
-                <CategoryFilter
-                    category={ this.props.filter.category }
-                    setFilter={ this.props.setFilter }
-                />
-                <ProviderFilter
-                    provider={ this.props.filter.provider }
-                    setFilter={ this.props.setFilter }
-                />
-                <ChannelFilter
-                    channel={ this.props.filter.channel }
-                    setFilter={ this.props.setFilter }
-                />
-                <ResetFilter
-                    setFilter={ this.props.setFilter }
-                />
-            </form>
+            <div className="wrapper wrapper--thin">
+                <form className="game-filter">
+                    <FeaturedFilter
+                        featured={ this.props.filter.featured }
+                        setFilter={ this.props.setFilter }
+                    />
+                    <JurisdictionFilter
+                        jurisdiction={ this.props.filter.jurisdiction }
+                        setFilter={ this.props.setFilter }
+                    />
+                    <CategoryFilter
+                        category={ this.props.filter.category }
+                        setFilter={ this.props.setFilter }
+                    />
+                    <ProviderFilter
+                        provider={ this.props.filter.provider }
+                        setFilter={ this.props.setFilter }
+                    />
+                    <ChannelFilter
+                        channel={ this.props.filter.channel }
+                        setFilter={ this.props.setFilter }
+                    />
+                    <ResetFilter
+                        setFilter={ this.props.setFilter }
+                    />
+                </form>
+            </div>
         );
     }
 }
